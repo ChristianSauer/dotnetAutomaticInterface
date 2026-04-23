simonmckenzie/net_automatic_interface

Fully qualify references to generated interfaces
This addresses an issue where, when a class references a generated interface, those references are not fully qualified, resulting in an interface that doesn't compile.

The change is to precalculate the list of interface names that will be generated, and, during symbol string generation, replace unrecognised symbols with generated names _where a single unambiguous match can be made between the symbol and the list of interfaces being generated_.

AutomaticInterface/AutomaticInterface/AutomaticInterfaceGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -39,13 +40,19 @@ public class AutomaticInterfaceGenerator : IIncrementalGenerator
             return;
         }
 
+        var interfaceNames = enumerations
+            .Select(Builder.GetInterfaceNameFor)
+            .Where(name => name != null)
+            .Cast<string>()
+            .ToList();
+
         foreach (var type in enumerations)
         {
             var typeNamespace = type.ContainingNamespace.IsGlobalNamespace
                 ? $"${Guid.NewGuid()}"
                 : $"{type.ContainingNamespace}";
 
-            var code = Builder.BuildInterfaceFor(type);
+            var code = Builder.BuildInterfaceFor(type, interfaceNames);
 
             var hintName = $"{typeNamespace}.I{type.Name}";
             context.AddSource(hintName, code);

ChristianSauer/dotnetAutomaticInterface

AutomaticInterface/DotnetAutomaticInterface/AutomaticInterfaceGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -40,13 +41,19 @@ public class AutomaticInterfaceGenerator : IIncrementalGenerator
             return;
         }
 
+        var generatedInterfaceNames = enumerations
+            .Select(Builder.GetInterfaceNameFor)
+            .Where(name => name != null)
+            .Cast<string>()
+            .ToList();
+
         foreach (var type in enumerations)
         {
             var typeNamespace = type.ContainingNamespace.IsGlobalNamespace
                 ? $"${Guid.NewGuid()}"
                 : $"{type.ContainingNamespace}";
 
-            var code = Builder.BuildInterfaceFor(type);
+            var code = Builder.BuildInterfaceFor(type, generatedInterfaceNames);
 
             var hintName = $"{typeNamespace}.I{type.Name}";
             context.AddSource(hintName, code);